e4376ff7df73fa82813302f0e558f123712013a3
Justin Noel <justin.noel@calendee.com>
2018-02-13 05:09:20 -0600

    feat: set default node version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import {
-  AfterContentInit, Component, ComponentFactoryResolver, ComponentRef, TemplateRef, ViewChild,
-  ViewContainerRef
-} from '@angular/core';
+import { Component, TemplateRef, ComponentRef, ViewContainerRef, ViewChild, AfterContentInit, ComponentFactoryResolver } from '@angular/core';
 
 import { AuthFormComponent } from './auth-form/auth-form.component';
 
@@ -10,22 +7,24 @@ import { User } from './auth-form/auth-form.interface';
 @Component({
   selector: 'app-root',
   template: `
-    <template #tmpl>
-      Justin Noel: USA
-    </template>
-
+    <div>
+      <div #entry></div>
+      <template #tmpl let-name let-location="location">
+        {{ name }} : {{ location }}
+      </template>
+    </div>
   `
 })
-export class AppComponent implements  AfterContentInit {
-
-  component: ComponentRef<AuthFormComponent>;
+export class AppComponent implements AfterContentInit {
 
   @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
   @ViewChild('tmpl') tmpl: TemplateRef<any>;
 
   ngAfterContentInit() {
-
-    this.entry.createEmbeddedView(this.tmpl);
+    this.entry.createEmbeddedView(this.tmpl, {
+      $implicit: 'Justin Noel',
+      location: 'Tx, USA'
+    });
   }
 
 }
